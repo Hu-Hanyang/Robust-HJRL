35ae5ca1814b841f01f90c6cb3e01a13ee469378
hha160 <HuHY97@outlook.com>
2024-03-12 15:28:02 -0700

    update the README.md

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="Add disturbance to the Reinforcement Learning method in the simulation environments for the CrazyFlie quadrotor. This repository is based on the https://github.com/SvenGronauer/phoenix-drone-simulation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Hu-Hanyang/robust-rl-with-HJ-distb.git",
+    url="git@github.com:Hu-Hanyang/gym-pybullet-drones.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -20,7 +20,7 @@ setuptools.setup(
     ],
     tests_require=['nose'],
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     include_package_data=True,
     # install_requires=[
     #     'numpy',
