69755a06a9bb37ce9b724b39e60d0603672cf8c6
hha160 <HuHY97@outlook.com>
2024-04-12 11:41:41 -0700

    try fixed=0.0 distb

--- gym_pybullet_drones/envs/HoverDistb.py
+++ gym_pybullet_drones/envs/HoverDistb.py
@@ -214,7 +214,7 @@ class HoverFixedDistbEnv(HoverDistbEnv):
     def __init__(self, *args,  **kwargs):  # distb_level=1.0, randomization_reset=False,
         # Set disturbance_type to 'fixed' regardless of the input
         kwargs['disturbance_type'] = 'fixed'
-        kwargs['distb_level'] = 1.0
+        kwargs['distb_level'] = 0.0
         kwargs['randomization_reset'] = True
         kwargs['record'] = True
         super().__init__(*args, **kwargs)  # distb_level=distb_level, randomization_reset=randomization_reset,
